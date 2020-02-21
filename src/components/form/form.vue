dae282cdc72473174b13436114ece1953a45f037
leowang <leowang@gmail.com>
2020-02-21 18:10:39 +0800

    新增form validate判斷

---
+++ src/components/form/form.vue
@@ -0,0 +1,44 @@
+<template>
+  <form>
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'iForm',
+  provide () {
+    return {
+      form: this
+    }
+  },
+  props: {
+    model: {
+      type: Object
+    },
+    rules: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      fields: []
+    }
+  },
+  created () {
+    this.$on('on-form-item-add', (field) => {
+      console.log(field)
+      if (field) this.fields.push(field)
+    })
+
+    this.$on('on-form-item-remove', (field) => {
+      console.log(this.fields.indexOf(field))
+      if (field.prop) this.fields.splice(this.fields.indexOf(field), 1)
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
